cad5341a4ac1d7ba5d899bbfb9308e8bc304320f
Romário Freitas <romarioaf1989@gmail.com>
2019-07-07 18:00:21 -0300

    Adding TeamController

---
+++ src/main/java/br/com/teamregister/controller/TeamController.java
@@ -0,0 +1,42 @@
+package br.com.teamregister.controller;
+
+import br.com.teamregister.model.Team;
+import br.com.teamregister.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class TeamController {
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @GetMapping("/teams")
+    public List<Team> getTeams() {
+        return (List<Team>) teamRepository.findAll();
+    }
+
+    @GetMapping("/teams/{id}")
+    public Team getByTeam(@PathVariable Long id) {
+        Optional<Team> team = teamRepository.findById(id);
+
+        if(team.isPresent())
+            return team.get();
+
+        return null;
+    }
+
+    @PostMapping("/teams")
+    public Team save(@RequestBody Team team) {
+       return teamRepository.save(team);
+    }
+
+    @PutMapping("/teams/{id}")
+    public Team update(@RequestBody Team team, @PathVariable Long id) {
+        team.setId(id);
+        return teamRepository.save(team);
+    }
+}
