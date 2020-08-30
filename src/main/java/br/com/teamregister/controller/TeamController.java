dde3f9e6790493bf4bda40a0e73ecc112b4e276f
Romário Freitas <romarioaf1989@gmail.com>
2020-08-30 18:38:58 -0300

    Adding maven wrapper

--- src/main/java/br/com/teamregister/controller/TeamController.java
+++ src/main/java/br/com/teamregister/controller/TeamController.java
@@ -3,9 +3,17 @@ package br.com.teamregister.controller;
 import br.com.teamregister.model.Player;
 import br.com.teamregister.model.Team;
 import br.com.teamregister.repository.TeamRepository;
+import br.com.teamregister.service.S3ClientFacade;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +22,9 @@ public class TeamController {
 
     @Autowired
     private TeamRepository teamRepository;
+
+    @Autowired
+    private S3ClientFacade s3ClientFacade;
 
     @GetMapping("/teams")
     public List<Team> getTeams() {
@@ -49,5 +60,23 @@ public class TeamController {
     public Team update(@RequestBody Team team, @PathVariable Long id) {
         team.setId(id);
         return teamRepository.save(team);
+    }
+
+    @PostMapping(value = "/teams/image", consumes = "multipart/form-data")
+    public S3FileWrapper updateImage(@RequestParam("file") MultipartFile file) throws IOException {
+
+        Path path = Paths.get("/tmp/myfile.txt");
+        Files.write(path, file.getBytes());
+
+        return new S3FileWrapper(s3ClientFacade.saveImage(path.toFile()));
+    }
+
+    @Getter @Setter
+    class S3FileWrapper {
+        private String key;
+
+        S3FileWrapper(String key) {
+            this.key = key;
+        }
     }
 }
